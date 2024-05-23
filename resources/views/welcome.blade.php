95fd6f91ed202545a3dc7eb6e01d62c8bc9a4c5f
NONYA ANGO <84583137+NONYAANGGOYUBO@users.noreply.github.com>
2024-05-23 10:07:02 +0530

    added marquee tag
    added marquee tag caption top to bottom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
     <h1>19th generation</h1>
     <h1>sdfasdkf</h1>
     <marquee behavior="" direction="">hahahaahha</marquee>
+    <marquee behavior="" direction="">top to bottom</marquee>
 
 </body>
 </html>
